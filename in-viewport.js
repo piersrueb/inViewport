aa5b208a9d7afe92a40183867ff6fa99897fe2fe
Piers Rueb <prueb@ukl48w9n12.burberry.corp>
2018-04-17 17:05:41 +0100

    init

--- in-viewport.js
+++ in-viewport.js
@@ -1,18 +1,18 @@
-//  in viewport
+//  in viewport js
 
-var allSections = document.getElementsByTagName('section'),
-    windowHeight = window.innerHeight;
+var allSections = document.getElementsByTagName('section'),  //  store all the section tags in an array
+    windowHeight = window.innerHeight;  //  get the window height
 
-window.addEventListener('scroll', inViewport);
+window.addEventListener('scroll', inViewport);  //  trigger the funtion on scroll
 
 function inViewport(){
-    for (var i = 0; i < allSections.length; i++) {
-        var viewportOffset = allSections[i].getBoundingClientRect(),
-            top = viewportOffset.top;
-        if(top < windowHeight){
-            allSections[i].setAttribute('class', 'in-viewport');
+    for (var i = 0; i < allSections.length; i++) {  //  loop through the sections
+        var viewportOffset = allSections[i].getBoundingClientRect(),  //  returns the size of an element and its position relative to the viewport
+            top = viewportOffset.top;  //  get the offset top
+        if(top < windowHeight){  //  if the top offset is less than the window height
+            allSections[i].setAttribute('class', 'in-viewport');  //  add the class
         }
     }
 }
 
-inViewport();
+inViewport();  //  run the function on init
