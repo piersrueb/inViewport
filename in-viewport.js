e618cf6ce9cc137ac2806eac220c31176d108372
piersrueb <piersrueb@gmail.com>
2018-09-26 16:06:39 +0100

    init

--- in-viewport.js
+++ in-viewport.js
@@ -1,18 +1,23 @@
 //  in viewport js
+//  Author Piers Rueb
+//  https://github.com/piersrueb/inViewport
 
-var allSections = document.getElementsByTagName('section'),  //  store all the section tags in an array
-    windowHeight = window.innerHeight;  //  get the window height
-
-window.addEventListener('scroll', inViewport);  //  run the funtion on scroll
-
-function inViewport(){
-    for (var i = 0; i < allSections.length; i++) {  //  loop through the sections
-        var viewportOffset = allSections[i].getBoundingClientRect(),  //  returns the size of an element and its position relative to the viewport
-            top = viewportOffset.top;  //  get the offset top
-        if(top < windowHeight){  //  if the top offset is less than the window height
-            allSections[i].classList.add('in-viewport');  //  add the class
+function inViewport(elem){
+    var allElements = document.getElementsByTagName(elem),
+        windowHeight = window.innerHeight;
+    window.addEventListener('scroll', elems);
+    function elems(){
+        for (var i = 0; i < allElements.length; i++) {  //  loop through the sections
+            var viewportOffset = allElements[i].getBoundingClientRect(),  //  returns the size of an element and its position relative to the viewport
+                top = viewportOffset.top;  //  get the offset top
+            if(top < windowHeight){  //  if the top offset is less than the window height
+                allElements[i].classList.add('in-viewport');  //  add the class
+            } else{
+                allElements[i].classList.remove('in-viewport');  //  add the class
+            }
         }
     }
+    elems();
 }
 
-inViewport();  //  run the function on init
+inViewport('section');  //  run the function on all section elements
