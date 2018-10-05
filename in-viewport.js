d58dc349fd3786462b39c9bc9411a306fe5bb8e2
piersrueb <piersrueb@gmail.com>
2018-10-05 11:14:24 +0100

    init

--- in-viewport.js
+++ in-viewport.js
@@ -2,14 +2,14 @@
 //  Author Piers Rueb
 //  https://github.com/piersrueb/inViewport
 
-function inViewport(elem){
-    var allElements = document.getElementsByTagName(elem),
-        windowHeight = window.innerHeight;
+const inViewport = (elem) => {
+    let allElements = document.getElementsByTagName(elem);
+    let windowHeight = window.innerHeight;
     window.addEventListener('scroll', elems);
-    function elems(){
-        for (var i = 0; i < allElements.length; i++) {  //  loop through the sections
-            var viewportOffset = allElements[i].getBoundingClientRect(),  //  returns the size of an element and its position relative to the viewport
-                top = viewportOffset.top;  //  get the offset top
+    const elems = () => {
+        for (let i = 0; i < allElements.length; i++) {  //  loop through the sections
+            let viewportOffset = allElements[i].getBoundingClientRect();  //  returns the size of an element and its position relative to the viewport
+            let top = viewportOffset.top;  //  get the offset top
             if(top < windowHeight){  //  if the top offset is less than the window height
                 allElements[i].classList.add('in-viewport');  //  add the class
             } else{
